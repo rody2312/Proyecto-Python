00baeb8cbc97ed7d2ffec1670e5fd4ed263c522d
rody2312 <84883025+rody2312@users.noreply.github.com>
2022-03-30 15:14:02 -0300

    Se agrego core y mysql

---
+++ core/db.py
@@ -0,0 +1,9 @@
+MYSQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bdd-scc',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '3306'
+    }
+}
