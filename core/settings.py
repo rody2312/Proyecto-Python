5b2fbfe11e9deca14978132b42e4b33fda4873ef
rody2312 <84883025+rody2312@users.noreply.github.com>
2022-04-12 18:56:58 -0400

    mysqlclient agregado a requirements y secret key modificado

--- core/settings.py
+++ core/settings.py
@@ -8,7 +8,8 @@ env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+""" BASE_DIR = Path(__file__).resolve().parent.parent """
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
