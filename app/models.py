652e366ac6810020ade80d21ceb07cbcb8ebad3a
Rafael <rafaelavaloscortes@gmail.com>
2022-05-24 22:30:31 -0400

    Evaluacion 70% faltan acciones

--- app/models.py
+++ app/models.py
@@ -101,11 +101,9 @@ class Notificacion(models.Model):
         db_table = 'notificacion'
 
 class Evaluacion(models.Model):
-    id_usuario = models.ForeignKey(Usuario, models.DO_NOTHING, db_column='id_usuario')
-    numero = models.TextField(null=False)
     titulo = models.TextField(null=False)
-    detalles = models.TextField(null=False)
-    fecha = models.DateField(auto_now=True, null=False)
+    descripcion = models.TextField(null=False)
+    fecha = models.DateField(null=False)
 
     class Meta():
         managed = True
--- app/views/views_evaluacion.py
+++ app/views/views_evaluacion.py
@@ -1,30 +1,77 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
-from django.views.generic import View
-#from ..forms import EvaluacionCreateForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import View, DeleteView
+from ..forms import EvaluacionCreateForm
 from app import forms
+from django.contrib import messages
 
 from app.models import Evaluacion
+
+
+#LISTAR EVALUACION
 
 class EvaluacionListView(LoginRequiredMixin ,View):
     
     def get(self,request, *args, **kwargs):
-        evaluacion = Evaluacion.objects.all()
+        evaluaciones = Evaluacion.objects.all()
         context={
-            'evaluacion': evaluacion,
-            'titulo': 'Evaluacion'
+            'evaluaciones': evaluaciones,
+            'titulo': 'Evaluaciones'
         }
         return render(request, 'evaluacion/evaluacion_list.html', context)
 
+#CREAR EVALUACION
 
-#class EvaluacionCreateView(LoginRequiredMixin ,View):
-    #def get(self,request, *args, **kwargs):
-        #form=EvaluacionCreateForm()
-        #context={
-            #'form': form,
-            #'titulo': 'Crear Evaluacion'
-        #}
-        #return render(request, 'evaluacion/evaluacion_create.html', context)
+class EvaluacionCreateView(LoginRequiredMixin ,View):
+    def get(self,request, *args, **kwargs):
+        form=EvaluacionCreateForm
+        context={
+            'form': form,
+            'titulo': 'Crear Evaluacion'
+        }
+        return render(request, 'evaluacion/evaluacion_create.html', context)
+
+    def post(self, request,*args, **kwargs):
+        if request.method=="POST":
+            form = EvaluacionCreateForm(request.POST)
+            if form.is_valid():
+                titulo = form.cleaned_data.get('titulo')
+                descripcion = form.cleaned_data.get('descripcion')
+                fecha = form.cleaned_data.get('fecha')
+                
+                u, created = Evaluacion.objects.get_or_create(titulo=titulo, descripcion=descripcion, fecha=fecha)
+                u.save()
+
+                messages.success(request, "Evaluacion agregada correctamente")
+                return redirect('app:evaluacion')
+
+
+# ELIMINAR EVALUACION                
+
+class EvaluacionDeleteView(LoginRequiredMixin, DeleteView):
+    model = Evaluacion
+    success_url = reverse_lazy('app:evaluacion_delete')
+
+    def get_success_url(self):
+        messages.success(self.request, "Eliminada correctamente")
+        return reverse('app:evaluacion_delete')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        success_url = self.get_success_url()
+        self.object.delete()
+        return HttpResponseRedirect(success_url)
+
+
+
+
+
+
+
+
+
 
 
 #class EvaluacionDetailsView(LoginRequiredMixin ,View):
@@ -35,16 +82,4 @@ class EvaluacionListView(LoginRequiredMixin ,View):
         #}
        # return render(request, 'evaluacion/evaluacion_details.html', context)
 
-
-
-#BUSCAR COMO HACER EL FORMULARIO
-
-#class EvaluacionFormView(LoginRequiredMixin ,View):
-    #def get(self,request, *args, **kwargs):
-        #form=EvaluacionCreateForm()
-        #context={
-            #'form': form,
-            #'titulo': 'Crear Evaluacion'
-        #}
-        #return render(request, 'evaluacion/evaluacion_create.html', context)
 
