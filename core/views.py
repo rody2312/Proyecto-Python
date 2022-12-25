54c7f0d624baec9f7e3b4535a69570c665cff461
rody2312 <84883025+rody2312@users.noreply.github.com>
2022-12-25 17:16:29 -0300

    Vista de alumno en Home

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic import View
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from app.models import Notificacion, NotificacionEnviada, Usuario
 from asistencia.models import UsuarioAsistencia
@@ -30,56 +30,97 @@ class HomeView(LoginRequiredMixin, View):
         #Obtiene todos los usuarios alumno
         alumnos = Usuario.objects.filter(id_tipo_usuario=3)
 
+        
+        #Se itera cada alumno para ir agregando 1 por 1 a la lista de alumnos que se mostrara en el template
+        fila = []
 
-        if Evaluacion.objects.filter(fecha=fecha).exists():
-            columnas.append('Evaluación')
-            
-            for a in alumnos:
-                alumno=[]
-                alumno.append(a.nombre)
-                alumno.append(a.apellido_paterno)
-                alumno.append(a.apellido_materno)
 
-                puntajeEv = UsuarioEvaluacion.objects.filter(usuario_id=a.id,evaluacion__fecha=fecha)
+        if request.user.id_tipo_usuario.id == 3:
 
-                if puntajeEv:
-                    alumno.append(puntajeEv.values('puntaje_id__puntaje')[0]['puntaje_id__puntaje'])
-                else:
-                    alumno.append('0')
-                
-                alumnosListEv.append(alumno)
+            #Obtiene al alumno alumno
+            alumno = get_object_or_404(Usuario, pk=request.user.id)
 
-        else:
             tipoActividades = TipoActividad.objects.all()
             columnas.append('Asistencia')
             for x in tipoActividades:
                 columnas.append(x.tipo)
+            columnas.append('Evaluación')
 
-            #Se itera cada alumno para ir agregando 1 por 1 a la lista de alumnos que se mostrara en el template
-            for a in alumnos:
-                alumno=[]
-                alumno.append(a.nombre)
-                alumno.append(a.apellido_paterno)
-                alumno.append(a.apellido_materno)
-                #Obtiene el puntaje de asistencia del alumno actual, y lo agrega a la variable
-                if UsuarioAsistencia.objects.filter(usuario_id=a.id,asistencia__fecha=fecha).exists():
-                    alumno.append(UsuarioAsistencia.objects.filter(usuario_id=a.id,asistencia__fecha=fecha).values('tipo_asistencia__puntaje')[0]['tipo_asistencia__puntaje'])
+
+            #Obtiene el puntaje de asistencia del alumno actual, y lo agrega a la variable
+            if UsuarioAsistencia.objects.filter(usuario_id=alumno.id,asistencia__fecha=fecha).exists():
+                fila.append(UsuarioAsistencia.objects.filter(usuario_id=alumno.id,asistencia__fecha=fecha).values('tipo_asistencia__puntaje')[0]['tipo_asistencia__puntaje'])
+            else:
+                fila.append('0')
+            #Crear lista de puntajes de todas las actividades
+            puntajeList= []
+            for t in tipoActividades:
+                if TipoActividad.objects.filter(pk=t.id, actividades_tipos__usuarioactividad__usuario_id=alumno.id,actividades_tipos__fecha=fecha).exists():
+                    puntajes = TipoActividad.objects.filter(pk=t.id, actividades_tipos__usuarioactividad__usuario_id=alumno.id,actividades_tipos__fecha=fecha).values(
+                        'actividades_tipos__usuarioactividad__puntaje_id__puntaje')[0]['actividades_tipos__usuarioactividad__puntaje_id__puntaje']
                 else:
-                    alumno.append('0')
+                    puntajes = '0'
+                puntajeList.append(puntajes)
+            fila.append(puntajeList)
+            if Evaluacion.objects.filter(fecha=fecha).exists():
+                puntajeEv = UsuarioEvaluacion.objects.filter(usuario_id=alumno.id,evaluacion__fecha=fecha)
+                if puntajeEv:
+                    fila.append(puntajeEv.values('puntaje_id__puntaje')[0]['puntaje_id__puntaje'])
+                else:
+                    fila.append('0')
+            else:
+                fila.append('0')
 
-                #Crear lista de puntajes de todas las actividades
-                puntajeList= []
+        else:
+            if Evaluacion.objects.filter(fecha=fecha).exists():
+                columnas.append('Evaluación')
 
-                for t in tipoActividades:
-                    if TipoActividad.objects.filter(pk=t.id, actividades_tipos__usuarioactividad__usuario_id=a.id,actividades_tipos__fecha=fecha).exists():
-                        puntajes = TipoActividad.objects.filter(pk=t.id, actividades_tipos__usuarioactividad__usuario_id=a.id,actividades_tipos__fecha=fecha).values(
-                            'actividades_tipos__usuarioactividad__puntaje_id__puntaje')[0]['actividades_tipos__usuarioactividad__puntaje_id__puntaje']
+                for a in alumnos:
+                    alumno=[]
+                    alumno.append(a.nombre)
+                    alumno.append(a.apellido_paterno)
+                    alumno.append(a.apellido_materno)
+
+                    puntajeEv = UsuarioEvaluacion.objects.filter(usuario_id=a.id,evaluacion__fecha=fecha)
+
+                    if puntajeEv:
+                        alumno.append(puntajeEv.values('puntaje_id__puntaje')[0]['puntaje_id__puntaje'])
                     else:
-                        puntajes = '0'
-                    puntajeList.append(puntajes)
+                        alumno.append('0')
 
-                alumno.append(puntajeList)
-                alumnosList.append(alumno)
+                    alumnosListEv.append(alumno)
+
+            else:
+                tipoActividades = TipoActividad.objects.all()
+                columnas.append('Asistencia')
+                for x in tipoActividades:
+                    columnas.append(x.tipo)
+
+                #Se itera cada alumno para ir agregando 1 por 1 a la lista de alumnos que se mostrara en el template
+                for a in alumnos:
+                    alumno=[]
+                    alumno.append(a.nombre)
+                    alumno.append(a.apellido_paterno)
+                    alumno.append(a.apellido_materno)
+                    #Obtiene el puntaje de asistencia del alumno actual, y lo agrega a la variable
+                    if UsuarioAsistencia.objects.filter(usuario_id=a.id,asistencia__fecha=fecha).exists():
+                        alumno.append(UsuarioAsistencia.objects.filter(usuario_id=a.id,asistencia__fecha=fecha).values('tipo_asistencia__puntaje')[0]['tipo_asistencia__puntaje'])
+                    else:
+                        alumno.append('0')
+
+                    #Crear lista de puntajes de todas las actividades
+                    puntajeList= []
+
+                    for t in tipoActividades:
+                        if TipoActividad.objects.filter(pk=t.id, actividades_tipos__usuarioactividad__usuario_id=a.id,actividades_tipos__fecha=fecha).exists():
+                            puntajes = TipoActividad.objects.filter(pk=t.id, actividades_tipos__usuarioactividad__usuario_id=a.id,actividades_tipos__fecha=fecha).values(
+                                'actividades_tipos__usuarioactividad__puntaje_id__puntaje')[0]['actividades_tipos__usuarioactividad__puntaje_id__puntaje']
+                        else:
+                            puntajes = '0'
+                        puntajeList.append(puntajes)
+
+                    alumno.append(puntajeList)
+                    alumnosList.append(alumno)
 
 
         #Obtener el ultimo archivo subido
@@ -100,6 +141,7 @@ class HomeView(LoginRequiredMixin, View):
             'archivo': archivo,
             'foro': foro,
             'notificacion': notificacion,
+            'fila': fila,
         }
         return render(request, 'index.html', context)
 
